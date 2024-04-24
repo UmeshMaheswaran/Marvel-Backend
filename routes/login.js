00c26b52496f74a599322e91542d4ee902778fef
Umesh Maheswaran <umeshmaheswaran@MacBook-Pro-de-Umesh.local>
2024-04-24 21:36:31 +0200

    login and Signup done in back

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const Login = require("../models/Login");
+
 router.post("/users/login", async (req, res) => {
+  const SHA256 = require("crypto-js/sha256");
+  const encBase64 = require("crypto-js/enc-base64");
+  const uid2 = require("uid2");
+
+  const password = req.body.password;
+  const salt = uid2(16);
+  const hash = SHA256(password + salt).toString(encBase64);
+  const token = uid2(16);
+
   try {
     console.log(req.body);
 
@@ -10,15 +21,16 @@ router.post("/users/login", async (req, res) => {
       account: {
         usernme: req.body.username,
       },
-      token: String,
-      hash: String,
-      salt: String,
+      token: token,
+      hash: hash,
+      salt: salt,
     });
     await newLogin.save();
-    res.statut(201).json(newLogin);
-  } catch (error) {}
-
-  res.status(500).json({ message: error.message });
+    res.status(201).json(newLogin);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -4,21 +4,31 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.post("/users/signup", async (req, res) => {
+  const SHA256 = require("crypto-js/sha256");
+  const encBase64 = require("crypto-js/enc-base64");
+  const uid2 = require("uid2");
+
+  const password = req.body.password;
+  const salt = uid2(16);
+  const hash = SHA256(password + salt).toString(encBase64);
+  const token = uid2(16);
+
   try {
     console.log(req.body);
 
     const newUser = new User({
-      email: String,
+      email: req.body.email,
       account: {
-        username: String,
+        username: req.body.username,
       },
-      token: String,
-      hash: String,
-      salt: String,
+      token: token,
+      hash: hash,
+      salt: salt,
     });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 });
