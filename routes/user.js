3782d5b359a79ef98836b973fdcdfc5b91882e8b
Umesh Maheswaran <umeshmaheswaran@MacBook-Pro-de-Umesh.local>
2024-05-02 19:19:48 +0200

    done login

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User");
+
 const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
 
 router.post("/users/signup", async (req, res) => {
   try {
@@ -11,14 +14,9 @@ router.post("/users/signup", async (req, res) => {
       const user = await User.findOne({ email: email });
 
       if (user === null) {
-        const SHA256 = require("crypto-js/sha256");
-        const encBase64 = require("crypto-js/enc-base64");
-        const uid2 = require("uid2");
-
         const salt = uid2(16);
         const hash = SHA256(password + salt).toString(encBase64);
         const token = uid2(64);
-
         console.log(req.body);
 
         const newUser = new User({
